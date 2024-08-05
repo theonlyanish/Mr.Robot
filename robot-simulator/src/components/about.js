710a3d13ab1547b97641522cef5c119f65e0da80
Anish <akap0009@student.monash.edu>
2024-08-05 17:48:04 +1000

    added an about me page

--- robot-simulator/src/components/about.js
+++ robot-simulator/src/components/about.js
@@ -1,11 +1,23 @@
 const About = () => {
+ 
+  const fontStyle = {
+    fontFamily: 'GTUltra, Lato, Noto Sans, Noto Sans JP, Noto Sans KR, Noto Sans SC, Noto Sans TC, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", Segoe UI Symbol, "Noto Color Emoji"',
+    fontSize: '.9285714285714286rem',
+    lineHeight: '1.5rem',
+    fontWeight: 400,
+    letterSpacing: '.025em',
+    WebkitFontSmoothing: 'antialiased'
+  };
+
   return (
-    <main style={{ margin: '40px', fontSize: '18px', lineHeight: '1.6', maxWidth: '800px', margin: '40px auto' }}>
+    <main style={{ margin: '40px auto', fontSize: '18px', lineHeight: '1.6', maxWidth: '800px' }}>
       <h1>About Me</h1>
-      <p>Hello, I'm Anish and this is a brief page about me.</p>
-      <p>This section is where you can talk about your interests, experiences, or any personal story you want to share with your visitors.</p>
-      <p>I'm passionate about robotics and programming, which led me to create this Robot Simulator project. It's a fun way to demonstrate basic concepts of robot movement and control in a 2D space.</p>
-      <p>Feel free to explore the simulator and get in touch if you have any questions or suggestions!</p>
+      <p style={fontStyle}>Hello, I'm Anish Kapse. Welcome to my personal space on the web where I share my passion for technology and innovation.</p>
+      <p style={fontStyle}>I am a software developer with a keen interest in front-end technologies and user experience design. I have experience in a range of programming languages and frameworks including JavaScript, TypeScript, React, and Node.js. My technical skills also extend to building responsive and accessible web applications that adhere to modern web standards.</p>
+      <p style={fontStyle}>My recent projects include a sophisticated meeting scheduler application that integrates real-time availability with automated notifications and seamless video conferencing capabilities. Additionally, I developed a centralized system for inventory management and recruitment that leverages the power of React and Google Cloud Platform to deliver a high-performance web experience.</p>
+      <p style={fontStyle}>Aside from my professional work, I'm also passionate about robotics and programming. This interest led me to create the Robot Simulator project, a fun and interactive way to demonstrate basic concepts of robot movement and control in a 2D space. This project is a reflection of my enthusiasm for merging creative problem solving with technical expertise.</p>
+      <p style={fontStyle}>Furthermore, I have developed <a href="https://apps.apple.com/au/app/healthtonic-health-tracker/id6496689413" style={{ textDecoration: 'none', color: 'blue' }}>HealthTonic</a>, an iOS application available on the App Store. This app is designed for detailed tracking of calories, steps, water intake, and weight, featuring personalized UI, barcode scanning, and integration with the Health app.</p>
+      <p style={fontStyle}>Feel free to explore my projects and connect with me if you have any questions or suggestions. Visit my <a href="https://github.com/theonlyanish" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a> to see more of my work or check out my coding snippets on <a href="https://codepen.io/akap0009" style={{ textDecoration: 'none', color: 'blue' }}>CodePen</a>.</p>
     </main>
   );
 };
