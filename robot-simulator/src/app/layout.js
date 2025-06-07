879a522f839d8aab6ce1c99d5cfaa46b58a86ecd
Anish <akap0009@student.monash.edu>
2025-06-07 18:45:50 +1000

    favicon

--- robot-simulator/src/app/layout.js
+++ robot-simulator/src/app/layout.js
@@ -3,13 +3,25 @@ import { DarkModeProvider } from '../components/DarkModeContext';
 import './globals.css';
 
 export const metadata = {
-  title: 'Robot Simulator',
+  title: 'RoboGrid',
   description: 'Control a robot on a 5x5 grid',
+  icons: {
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon-16x16.png',
+    apple: '/favicon/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body>
         <DarkModeProvider>
           <Header />
