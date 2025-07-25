87a66262ba35b4e924eab512932c729d746acba2
Anish <akap0009@student.monash.edu>
2025-07-25 14:20:50 +1000

    favicon fix + redundant icon removal

--- robot-simulator/src/app/layout.js
+++ robot-simulator/src/app/layout.js
@@ -6,9 +6,9 @@ export const metadata = {
   title: 'RoboGrid',
   description: 'Control a robot on a 5x5 grid',
   icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
+    icon: '/favicon_io/favicon.ico',
+    shortcut: '/favicon_io/favicon-16x16.png',
+    apple: '/favicon_io/apple-touch-icon.png',
   },
 };
 
@@ -16,16 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon/favicon-16x16.png" type="image/png" sizes="16x16" />
-        <link rel="icon" href="/favicon/favicon-32x32.png" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="icon" href="/favicon_io/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon_io/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon_io/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
       </head>
       <body>
         <DarkModeProvider>
-          <Header />
-          {children}
+        <Header />
+        {children}
         </DarkModeProvider>
       </body>
     </html>
